eda131e696a61be9a9f91b4e85061fa5b2e16105
Louis Chan <louischan0325@gmail.com>
2020-01-30 21:45:38 +0800

    Updates

---
+++ precache-manifest.44ba6dcd74c04a94c104787021c30265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e836107cab0f89f4e83e21e4422ec345",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "6a885cf498398d19062a",
+    "url": "/dice/static/css/main.491dc812.chunk.css"
+  },
+  {
+    "revision": "f856ac10d10c02b1fd0c",
+    "url": "/dice/static/js/2.8dce97e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dice/static/js/2.8dce97e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a885cf498398d19062a",
+    "url": "/dice/static/js/main.bf919374.chunk.js"
+  },
+  {
+    "revision": "14b2aa6c4418acc9fea8",
+    "url": "/dice/static/js/runtime-main.753b5fde.js"
+  }
+]);
